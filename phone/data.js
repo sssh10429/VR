c9a30c7ca91ec43a3391f0b583816e0dc7bf9be9
sssh10429 <brian85317@gmail.com>
2024-11-28 11:57:25 +0800

    11/28

---
+++ phone/data.js
@@ -0,0 +1,234 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nintendo-switch",
+      "name": "Nintendo Switch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -1.5690886228918792,
+        "pitch": 0.14237981840005887,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7210541184509154,
+          "pitch": 0.3257229793269225,
+          "rotation": 7.853981633974483,
+          "target": "1-pokmon-ga-ol"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.753544118994462,
+          "pitch": -0.05477161471021219,
+          "title": "Nintendo Switch",
+          "text": "有Switch專賣店"
+        }
+      ]
+    },
+    {
+      "id": "1-pokmon-ga-ol",
+      "name": "Pokémon ga-olé",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -1.5070673682220566,
+        "pitch": 0.13645549687173641,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7650510672222062,
+          "pitch": 0.3526421264658257,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        },
+        {
+          "yaw": -2.2924529283386708,
+          "pitch": 0.37387436712352873,
+          "rotation": 4.71238898038469,
+          "target": "0-nintendo-switch"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5361108141725417,
+          "pitch": -0.03274537276310241,
+          "title": "Pokémon ga-olé",
+          "text": "還有販賣Pokémon&nbsp;ga-olé 的卡片"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "手機配件",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.735531613340477,
+        "pitch": 0.09668191829852546,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12671586962623138,
+          "pitch": 0.207777921850548,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.7355311169704457,
+          "pitch": 0.6180675697153131,
+          "rotation": 3.9269908169872414,
+          "target": "1-pokmon-ga-ol"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7473454274282876,
+          "pitch": 0.07605989458357243,
+          "title": "手機配件",
+          "text": "手機殼、保護膜等周邊商品"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "動漫周邊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 1.3143478605073717,
+        "pitch": 0.11194748434565582,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0373475307571107,
+          "pitch": 0.44196421759586,
+          "rotation": 7.853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.5857162508788676,
+          "pitch": 0.34164666379571074,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3214635765099985,
+          "pitch": 0.14502287744776865,
+          "title": "動漫周邊",
+          "text": "本來其實有在賣得但是剛好沒有拍到"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "手機維修",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 1.5612696145083103,
+        "pitch": 0.1780982705499099,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1851504844252263,
+          "pitch": 0.437280353799391,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.408183455999609,
+          "pitch": 0.022956546341115214,
+          "title": "手機維修",
+          "text": "除了維修以外，更有販賣配件"
+        }
+      ]
+    }
+  ],
+  "name": "3C周邊",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
