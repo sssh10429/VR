250ad65eaf846281ee4b6c5f647725e8e474324e
sssh10429 <brian85317@gmail.com>
2024-11-28 11:27:09 +0800

    11/25

---
+++ app-files/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "車站封面 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5102440874399834,
+        "pitch": -0.03561965410997203,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.320979422431451,
+          "pitch": -0.4323900681727544,
+          "title": "台北車站",
+          "text": "台北最繁忙的地方<div>地下更有K區、中山、Y區等多條地下街</div>"
+        }
+      ]
+    }
+  ],
+  "name": "台北車站",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
