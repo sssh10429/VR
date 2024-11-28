31f9650129df53c5a9811d79c19c79f4c503dd1b
sssh10429 <brian85317@gmail.com>
2024-11-28 11:40:09 +0800

    11/28

---
+++ machine/data.js
@@ -0,0 +1,238 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-y17-",
+      "name": "Y17入口 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1571930114572027,
+        "pitch": -0.015265566047133916,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.189609632587782,
+          "pitch": -0.059390662759451374,
+          "rotation": 10.210176124166829,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1097982301362883,
+          "pitch": -0.4009602396447107,
+          "title": "Y17入口",
+          "text": "最好尋找不易迷路的路口"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "有很多機台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.5874221827349757,
+        "pitch": 0.20782126499035414,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.027464616758955174,
+          "pitch": 0.06451802124592199,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.08167518457585743,
+          "pitch": 0.48934162738916953,
+          "rotation": 3.141592653589793,
+          "target": "0-y17-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6677405735042274,
+          "pitch": -0.012378334132154833,
+          "title": "娃娃機台",
+          "text": "裡面有很多種類可以夾"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "扭蛋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.8887531121453591,
+        "pitch": 0.21118752933538332,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04229965259589363,
+          "pitch": 0.19241678328212153,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.26533952848788545,
+          "pitch": 0.630804113259428,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8477913031699948,
+          "pitch": 0.2264392296991531,
+          "title": "扭蛋區",
+          "text": "這邊扭蛋的店很多<div>但這邊的物品最好</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "保夾比較便宜的機台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.9581501403805568,
+        "pitch": 0.19081957558918816,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0681861616277395,
+          "pitch": -0.05736272559997069,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.45991976626136655,
+          "pitch": 0.6203036846106063,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3746865906823373,
+          "pitch": 0.13477302289193815,
+          "title": "娃娃機台",
+          "text": "這裡機台保夾很便宜"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "更多機台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.7052554936630102,
+        "pitch": 0.022898349070704427,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3266191885548686,
+          "pitch": 0.4105034399670586,
+          "rotation": 3.141592653589793,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2071298667482786,
+          "pitch": 0.0691222412577428,
+          "title": "更多的機台",
+          "text": "這邊有娃娃機、音遊、拳擊等多種機台"
+        }
+      ]
+    }
+  ],
+  "name": "機台區",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
